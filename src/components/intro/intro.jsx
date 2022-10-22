66388d60e3964854e43a7b779649dca6a4469d33
mdmoosasohail <mdmoosasohail8112@gmail.com>
2022-10-22 22:19:32 +0530

    first commit

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,25 @@
+import "./intro.css"
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                      <h2 className="i-intro">hello, my name is</h2>
+                      <h1 className="i-name"> md moosa sohail</h1>
+                      <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">web developer</div>
+                            <div className="i-title-item">manual tester</div>
+                            <div className="i-title-item">content creator</div>
+                            <div className="i-title-item">writer</div>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+            <div className="i-right">right</div>
+            
+        </div>
+    )
+}
+
+export default Intro
